7b9e1fcd207ad362d375656dec2e0f9740cbdbe1
guanhaofeng <zhushuishan@126.com>
2019-07-26 10:10:54 +0800

    ah

--- algorithms/src/main/java/Main.java
+++ algorithms/src/main/java/Main.java
@@ -1,7 +1,17 @@
-package PACKAGE_NAME;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 
 /**
  * @author ghf on 2019-07-23 12:32
  */
 public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+    }
 }
